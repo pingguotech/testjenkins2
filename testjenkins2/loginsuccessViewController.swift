a75a2de184e0666f5b5a3033c99b68f14417fc11
Ben <benton_chan@yahoo.ca>
2018-07-30 11:40:45 -0400

    storyboard loginsuccess

---
+++ testjenkins2/loginsuccessViewController.swift
@@ -0,0 +1,35 @@
+//
+//  loginsuccessViewController.swift
+//  testjenkins2
+//
+//  Created by Dev on 2018-07-30.
+//  Copyright © 2018 Dev. All rights reserved.
+//
+
+import UIKit
+
+class loginsuccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
